9473d5e6693a97103ec8fb41f7e13b3dc352de0c
Daniel K <turgandeminos@gmail.com>
2019-05-09 10:28:09 +0200

    Hello World v1

--- KlassGitOpgave/Program.cs
+++ KlassGitOpgave/Program.cs
@@ -10,6 +10,8 @@ namespace KlassGitOpgave
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
         }
     }
 }
