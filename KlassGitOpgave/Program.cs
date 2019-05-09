3f17d7363ccb2d17f7f0ccd774729d8d052099a8
Daniel Krongaard <38807992+turganscp@users.noreply.github.com>
2019-05-09 10:34:47 +0200

    Update Program.cs

--- KlassGitOpgave/Program.cs
+++ KlassGitOpgave/Program.cs
@@ -10,7 +10,7 @@ namespace KlassGitOpgave
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Worlds");
             Console.ReadLine();
         }
     }
